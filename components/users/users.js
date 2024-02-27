ea93e72c5e12fa78b5ea0958777ddd6ecc0e9d4f
Bilal Ozdemir <ozdemirbilal01@gmail.com>
2024-02-28 00:07:34 +0300

    fetch user information added.

--- components/users/users.js
+++ components/users/users.js
@@ -4,10 +4,22 @@ import { useState, useEffect } from 'react'
 function Users(){
     const[users, setUsers] = useState([]);
 
+    useEffect(() => {
+        fetch('http://jsonplaceholder.typicode.com/users')
+        .then((response) => response.json())
+        .then((data) => {
+            setUsers(data)
+            //isLoading ekle
+        })
+        .catch((e) => console.log(e))
+    }, []);
+
     return(
-       <div>
-        
-       </div> 
+        <div>
+
+            <h2>USERS</h2>
+
+        </div>
     )
 }
 
